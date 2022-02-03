f7af5a7b0fc32b944a36aa0799cba350d11bbf88
Emre Yoleri <emreyoleri01@gmail.com>
2022-02-04 00:00:31 +0300

    Navbar Design Completed

---
+++ src/components/Navbar/Announcement/Announcement.jsx
@@ -0,0 +1,7 @@
+import { Container } from "./AnnouncementStyles";
+
+const Announcement = () => {
+  return <Container>Super Deal! Free Shopping on Orers Over $50</Container>;
+};
+
+export default Announcement;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import { Badge } from "@mui/material";
+import { Search, ShoppingCartOutlined } from "@mui/icons-material";
+import {
+  Center,
+  Container,
+  Input,
+  Language,
+  Left,
+  Logo,
+  MenuItem,
+  Right,
+  Wrapper,
+  SearchContainer,
+} from "./NavbarStyles";
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>LAMA.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary" />
+            <ShoppingCartOutlined />
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
